ac78d9a2ffc02cb56648dd1b7efd2e56ffaac074
sosuisen <hidekazu.kubota@gmail.com>
2023-04-16 21:51:48 +0900

    fix: add @Named

--- src/main/java/com/example/model/User.java
+++ src/main/java/com/example/model/User.java
@@ -3,8 +3,10 @@ package com.example.model;
 import java.io.Serializable;
 
 import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
 
 @lombok.Data
+@Named
 @SessionScoped
 public class User implements Serializable {
 	private String name;
